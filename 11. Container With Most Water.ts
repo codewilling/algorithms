a774768ccbcc1efb0baa87bbdd6fee2efd8f0677
codewilling <codewilling@gmail.com>
2023-08-21 14:10:34 -0700

    add 11. Container With Most Water

---
+++ 11. Container With Most Water.ts
@@ -0,0 +1,46 @@
+//current number x number away
+//if number at index i is <= current number, multiply by pointer by current number
+// function maxArea(height: number[]): number {
+//   let maximum = 0;
+//   for (let i = 0; i < height.length; i++) {
+//     console.log(i);
+//     let pointer = 1;
+//     let nextNumber = height[i + pointer];
+//     if (nextNumber) {
+//       if (nextNumber <= height[i]) {
+//         let product = pointer * nextNumber;
+//         console.log(product);
+//         if (product > maximum) {
+//           maximum = product;
+//           console.log(maximum);
+//         }
+//       }
+//       nextNumber = height[i + pointer + 1];
+//       console.log(nextNumber);
+//       pointer++;
+//       console.log(pointer);
+//     }
+//   }
+//   return maximum;
+// }
+
+let height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+function maxArea(height: number[]) {
+  let leftPointer = 0;
+  let righPointer = height.length - 1;
+  let maximum = 0;
+  while (leftPointer < righPointer) {
+    let width = righPointer - leftPointer;
+    let product = width * Math.min(height[leftPointer], height[righPointer]);
+    maximum = Math.max(product, maximum);
+    if (height[leftPointer] < height[righPointer]) {
+      leftPointer++;
+    } else {
+      righPointer--;
+    }
+  }
+  return maximum;
+}
+
+console.log(maxArea(height));
